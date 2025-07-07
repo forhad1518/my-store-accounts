12147500dae5deff2c8b96f93390dc5165e3cae7
forhad1518 <ahforhad21@gmail.com>
2025-07-07 22:45:38 +0600

    cash out form from collect data

--- src/components/Cash In Out/CashOutForm.jsx
+++ src/components/Cash In Out/CashOutForm.jsx
@@ -1,6 +1,19 @@
 import React, { useEffect, useState } from 'react'
 
 export default function CashOutForm() {
+    /* Handle cash out form */
+    const cashOutformHandle = e => {
+        e.preventDefault()
+        /* Data from cash in form */
+        const date = e.target.date.value;
+        const cash_out_option = e.target.cash_out_option.value;
+        const amount = e.target.amount.value;
+        const description = e.target.description.value;
+
+        const all_data = [date, cash_out_option, amount, description]
+        console.log(all_data)
+
+    }
     /* Auto Local time set */
     const [localTime, setLocalTime] = useState('')
     useEffect(() => {
@@ -16,27 +29,27 @@ export default function CashOutForm() {
                 <p>Cash Out form</p>
             </div>
             <div>
-                <form action="">
+                <form onSubmit={cashOutformHandle} action="">
                     {/* Input Date & Time */}
                     <input
                         className="border p-2 rounded"
                         type="datetime-local"
                         defaultValue={localTime}
-                        name="" id=""
+                        name="date" id=""
                         required
                     />
                     {/* Select Option */}
-                    <select className="border p-2 rounded" required>
+                    <select name='cash_out_option' className="border p-2 rounded" required>
                         <option value="">Select Expensed</option>
-                        <option value="">Store</option>
-                        <option value="">Family</option>
-                        <option value="">Family 1</option>
-                        <option value="">Others</option>
+                        <option value="Store">Store</option>
+                        <option value="Family">Family</option>
+                        <option value="Family 1">Family 1</option>
+                        <option value="Others">Others</option>
                     </select>
                     {/* input Amount */}
-                    <input className="border p-2 rounded" placeholder='Amount/Taka' type="number" name="" id="" required />
+                    <input className="border p-2 rounded" placeholder='Amount/Taka' type="number" name="amount" id="" required />
                     {/* Description */}
-                    <input className="border p-2 rounded" placeholder='Description' type="text" name="" id="" required />
+                    <input className="border p-2 rounded" placeholder='Description' type="text" name="description" id="" required />
                     {/* Submit */}
                     <input className="border p-2 rounded" type="submit" value="Save" required />
                 </form>
